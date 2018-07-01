6d8740e48bd027a2d030dc69049e597402445254
SAMUELGAC <40730022+SAMUELGAC@users.noreply.github.com>
2018-07-01 07:56:00 +0200

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,12 @@
+--[[ 
+	ONEMenu
+	Application, themes and files manager.
+	
+	Licensed by Creative Commons Attribution-ShareAlike 4.0
+	http://creativecommons.org/licenses/by-sa/4.0/
+	
+	Designed By Gdljjrod & DevDavisNunez.
+	Collaborators: BaltazaR4 & Wzjk.
+]]
+
+dofile("resources/adrenalineupdate.lua")									
