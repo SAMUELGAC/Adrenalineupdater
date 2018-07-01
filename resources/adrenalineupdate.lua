796663c009230396dd85af6e9a66a3387a7353db
SAMUELGAC <40730022+SAMUELGAC@users.noreply.github.com>
2018-07-01 07:57:03 +0200

    v1.01

---
+++ resources/adrenalineupdate.lua
@@ -0,0 +1,30 @@
+------Autoupdate
+-------------------Resource-----------
+
+color.loadpalette() -- Load Defaults colors
+back = image.load("resources/back.png")
+
+while true do
+	buttons.read()
+	if back then back:blit(0,0) end
+	screen.flip() -- Show Buff
+
+	if buttons.cross then
+		files.extract("resources/Update.zip","ux0:app/PSPEMUCFW/sce_module")
+		os.message("Your PSVita will restart...\nAdrenaline has been updated",0)
+		os.delay(2500)
+	buttons.homepopup(1)
+	power.restart()
+end
+
+	if buttons.circle then
+              files.extract("resources/Update.zip","ur0:app/PSPEMUCFW/sce_module")
+		os.message("Your PSVita will restart...\nAdrenaline has been updated",0)
+		os.delay(2500)
+	buttons.homepopup(1)
+	power.restart()
+end
+
+
+	if buttons.triangle then break end -- Exit
+end
